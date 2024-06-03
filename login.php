9a7c268f0918b8a565707c46832aa603ea8fe15c
xKensho47 <e.damiantripodi@yahoo.com>
2024-06-03 12:24:36 -0300

    Detalles

--- login.php
+++ login.php
@@ -8,6 +8,8 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="css/normalize.css">
     <link rel="stylesheet" href="css/estilos.css">
+    <link rel="stylesheet" type="text/css" href="slick/slick.css" />
+    <link rel="stylesheet" type="text/css" href="slick/slick-theme.css" />
     <title>Log in</title>
 </head>
 
@@ -31,9 +33,10 @@
         ';
         ?>
     </div>
-    <script src="script/jquery.js"></script>
-    <script src="script/pop-ups.js"></script>
-    <script src="script/botonTop.js"></script>
+        <script src="script/jquery.js"></script>
+        <script src="slick/slick.min.js"></script>
+        <script src="script/script.js"></script>
+        <script src="script/botonTop.js"></script>
 
     <footer>
         <p>&copy; CineFlow 2024</p>
--- registro_process.php
+++ registro_process.php
@@ -2,6 +2,25 @@
 session_start();
 include('conexion.php');
 
+echo'
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/estilos.css">
+    <link rel="stylesheet" type="text/css" href="slick/slick.css" />
+    <link rel="stylesheet" type="text/css" href="slick/slick-theme.css" />
+    <title>Overlay Registro</title>
+</head>
+
+<script src="script/jquery.js"></script>
+<script src="slick/slick.min.js"></script>
+<script src="script/script.js"></script>
+<script src="script/botonTop.js"></script>
+';
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $nombre = $_POST['nombre'];
     $apellido = $_POST['apellido'];
