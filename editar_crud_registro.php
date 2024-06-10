f05a2264db4aff0157ea536e8227d0e8f7a8fd08
Agustina444 <alanosa4@gmail.com>
2024-06-09 23:53:00 -0300

    Mas Editar_datos_pelicula

--- editar_crud_registro.php
+++ editar_crud_registro.php
@@ -1,19 +1,28 @@
 <?php
 include("conexion.php");
 
-//si no esta vacio
-    if(!empty($_POST['nombre'])){   
+    //si no esta vacio
+    if(!empty($_POST['id_peli'])){
 
-        $titulo_pelicula = $conexion->real_escape_string($_POST['nombre']);
+        if (!empty($_POST['nombre']) || !empty($_POST['descripcion']) || !empty($_POST['estreno']) || !empty($_POST['duracion'])) {  
         $id_pelicula = $conexion->real_escape_string($_POST['id_peli']);
+        $titulo_pelicula = $conexion->real_escape_string($_POST['nombre']);
+        $descripcion_pelicula = $conexion->real_escape_string($_POST['descripcion']);
+        $estreno_pelicula = $conexion->real_escape_string($_POST['estreno']);
+        $duracion_pelicula = $conexion->real_escape_string($_POST['duracion']); 
 
         $consulta = ("UPDATE peliculas
-                       SET titulo= '$titulo_pelicula'
-                       WHERE id_peli= '$id_pelicula'"); 
+                        SET titulo = '$titulo_pelicula',
+                            descripcion = '$descripcion_pelicula',
+                                estreno = '$estreno_pelicula',
+                                    duracion = '$duracion_pelicula'
+                                        WHERE id_peli= '$id_pelicula'");               
             
         $conexion->query($consulta);
+
         header('Location: crud_peliculas.php?status=success');
         exit();
-    }
+        }
+    } 
 
 ?>
