853d89a83fc95b02702a522d23b49095f9c984de
jazmin707 <jazcar2003@gmail.com>
2024-06-16 11:45:59 -0300

    arreglo de imagen del index

--- verMasTardePerfil.php
+++ verMasTardePerfil.php
@@ -62,17 +62,17 @@ if(isset($_SESSION['id_cuenta'])){
             <div class="volver-atras" style="float: left; margin-left: 20px;">
                 <a href="profile.php" id="back-link" class="h2-animate"><i class="fas fa-arrow-left"></i>Perfil</a>
             </div>
-         <main class="contenedor-principal-main">
+         <main class="contenedor-principal-main animate-from-bottom">
             
             <h2 class="h2-animate">Ver más tarde</h2>
-            <div class="contenedor-peliculas">
+            <div class="contenedor-peliculas ">
                 <?php
                     if ($result && $result->num_rows > 0) {
                         while ($row = $result->fetch_assoc()) {
                             $imagePath = $row["poster"];
                             echo "
                                 <a href='detalle_peli.php?id_peli=" . $row['id'] . "'>
-                                    <img src=' " . $imagePath . " ' alt='Movie Posters'>
+                                    <img src=' " . $imagePath . " ' alt='Movie Posters' class='imagen-deslizar'>
                                 </a>
                                 ";
                         }
