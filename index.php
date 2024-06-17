876adef801d14dc64f9f01d91dd7ec3102cf3073
CagianesaEmiliano <129976905+CagianesaEmiliano@users.noreply.github.com>
2024-06-17 16:44:35 -0300

    Finalización del footer
    Footer terminado.

--- index.php
+++ index.php
@@ -196,9 +196,9 @@ include ('conexion.php');
         <script src="script/botonTop.js"></script>
         
     </div>
-    <footer>
-        <p>&copy; CineFlow 2024</p>
-    </footer>
+    <?php 
+    include('footer.php');
+    ?>
 </body>
 </html>
 
