90ff004ea87654b838f6356e73929a340dbf862f
xKensho47 <e.damiantripodi@yahoo.com>
2024-06-18 13:47:27 -0300

    Footer terminado

--- script/footerControl.js
+++ script/footerControl.js
@@ -1,17 +1,9 @@
-document.addEventListener("mousemove", function(event) {
+document.querySelector(".footer-toggle").addEventListener("mouseenter", function() {
     var footerElement = document.querySelector("footer");
-    
-    // Distancia entre el mouse y el borde inferior de la ventana
-    var distanceFromBottom = window.innerHeight - event.clientY;
-    
-    // Altura en px desde la parte inferior para mostrar el footer
-    var triggerDistance = 20; // Ejemplo: 20px desde abajo
-    
-    // Mostrar u ocultar el footer basado en la posición del mouse cerca del borde inferior
-    if (distanceFromBottom < triggerDistance) {
-        footerElement.style.display = "block"; /* Muestra el footer con la transición */
-        footerElement.style.bottom = "0"; /* Aparece con transición */
-    } else {
-        footerElement.style.display = "none"; /* Oculta el footer con la transición */
-    }
+    footerElement.style.transform = "translateY(0)"; /* Muestra el footer */
+});
+
+document.querySelector("footer").addEventListener("mouseleave", function() {
+    var footerElement = document.querySelector("footer");
+    footerElement.style.transform = "translateY(calc(100% - 3vh))"; /* Oculta el footer cuando el mouse deja el footer */
 });
