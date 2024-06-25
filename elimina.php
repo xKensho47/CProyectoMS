4dff4bf254f1b5fd11ff9e42ea68aba463c1302f
vivianaponce <vivianasofiaponce@gmail.com>
2024-06-24 23:26:28 -0300

    elimina peliculas con todas las relaciones

--- elimina.php
+++ elimina.php
@@ -1,29 +1,40 @@
 <?php
 include('conexion.php');
 
-    $id = $conexion->real_escape_string($_POST['pelicula_eliminar']);
+$id = $conexion->real_escape_string($_POST['pelicula_eliminar']);
 
-    $conexion->query("START TRANSACTION");
+// Iniciar la transacción
+$conexion->begin_transaction();
 
-    // Eliminar las relaciones en la tabla peli_genero
+try {
+    // Eliminar las relaciones en las tablas relacionadas
+    $conexion->query("DELETE FROM peli_like WHERE id_peli = $id");
+    $conexion->query("DELETE FROM mas_tarde WHERE id_peli = $id");
+    $conexion->query("DELETE FROM valoracion_peliculas WHERE id_peli = $id");
+    $conexion->query("DELETE FROM peli_estrellas WHERE id_peli = $id");
+    $conexion->query("DELETE FROM notificacion WHERE id_peli = $id");
+    $conexion->query("DELETE FROM peli_favorita WHERE id_peli = $id");
     $conexion->query("DELETE FROM peli_genero WHERE id_peli = $id");
-
-    // Eliminar las relaciones en la tabla peli_actor
     $conexion->query("DELETE FROM peli_actor WHERE id_peli = $id");
-
-    // Eliminar las relaciones en la tabla peli_director
     $conexion->query("DELETE FROM peli_director WHERE id_peli = $id");
 
     // Eliminar la película en la tabla peliculas
     $conexion->query("DELETE FROM peliculas WHERE id_peli = $id");
 
     // Confirmar la transacción
-    $conexion->query("COMMIT");
+    $conexion->commit();
 
-    // Cerrar la conexión
-    $conexion->close();
-
+    // Redirigir con éxito
     header('Location: crud_peliculas.php?status=success');
     exit();
+} catch (Exception $exception) {
+    // Revertir la transacción en caso de error
+    $conexion->rollback();
+    // Redirigir con mensaje de error genérico
+    header("Location: crud_peliculas.php?status=error");
+    exit();
+}
 
+// Cerrar la conexión
+$conexion->close();
 ?>
